68fb348d61d68dc9cfc2715e285b284ca1b20a50
Jonathan Bußhoff <jonathan@busshoff.net>
2021-01-09 13:40:49 +0100

    fix: usePrompt added to export

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import { DE, EN } from "./locales";
 import * as types from "./types";
 import { setLocaleMessages } from "./Localization";
 import PromptProvider from "./components/Prompt/PromptProvider";
+import { usePrompt } from "./components/Prompt/PromptContext";
 
 const DialogLocales = {
 	DE,
 	EN,
 };
 
-export { Dialog, DialogLocales, PromptProvider, setLocaleMessages, types };
+export { Dialog, DialogLocales, PromptProvider, usePrompt, setLocaleMessages, types };
